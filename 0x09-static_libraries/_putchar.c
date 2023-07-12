f1859fdff8d8af5cda233a2a34a65cb047d331f1
adengrail7@gmail.com <adengrail7@gmail.com>
2023-07-12 00:01:35 -0700

    Create the static library libmy.a containing all the functions

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n element of an array of integers
+ * @a: int to check
+ * @n: int to check
+ * Return: 0 is success
+ */
+int _putchar(char c)
+{
+}	
